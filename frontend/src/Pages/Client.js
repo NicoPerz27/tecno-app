c55a7ca80e9a5a80f964e043b6050abdb0d1ba08
felipevees <juanfelipevelasquezescobar82@gmail.com>
2023-05-12 17:36:49 -0500

    vista de cliente

--- frontend/src/Pages/Client.js
+++ frontend/src/Pages/Client.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import Cookies from "universal-cookie";
 import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
 const cookies = new Cookies();
 const URLCredits = "http://localhost:4000/credits";
@@ -29,31 +31,78 @@ export default class Client extends Component {
     window.location.href = "./LogIn";
   };
   getCredit = async (creditId) => {
-    console.log(creditId)
     const res = await axios.get(URLCredits + "/" + creditId);
     this.setState({ credit: res.data });
-    console.log(res);
-    //console.log(this.state.credit);
+    console.log(this.state.credit);
   };
   componentDidMount() {
-    if (cookies.get("rol") != "Client") {
+    if (cookies.get("rol") !== "Client") {
       window.location.href = "./LogIn";
     }
-<<<<<<< HEAD
-    console.log(cookies.get("CC"));
     this.getCredit(cookies.get("CC"));
-=======
-    this.getCredit(cookies.get('CC'))
->>>>>>> 5b573b105979e632718a28398529bd38436aa035
   }
-  //<h1>{this.state.credit.CC}</h1>
-  //<img src={this.state.credit.letterurl}></img>
+  componentDidCatch(error, errorInfo) {
+    console.log(error, errorInfo);
+  }
   render() {
     return (
-      <div>
-        <div className="title-client"></div>
+      <div
+        className="container my-3"
+        style={{
+          display: "grid",
+          justifyContent: "center",
+        }}
+      >
+        <div
+          className="card"
+          style={{
+            width: "70vw",
+            boxShadow: "0px 0px 3px rgba(0, 0, 0, 0.8)",
+          }}
+        >
+          <div className="card-body">
+            <h5 className="card-title" style={{ fontSize: "1.5rem" }}>
+              {this.state.credit.name} {this.state.credit.lastname}
+            </h5>
+            <p className="card-text">CC:{this.state.credit.CC}</p>
+          </div>
+          <div className="row">
+            <div className="col-sm-12 col-md-6 col-lg-6 col-xl-6">
+              <img
+                src={this.state.credit.permissurl}
+                alt="iphone"
+                className="img-fluid"
+              ></img>
+            </div>
+            <div className="col-sm-12 col-md-6 col-lg-6 col-xl-6">
+              <img
+                src={this.state.credit.letterurl}
+                alt="iphone-2"
+                className="img-fluid"
+              ></img>
+            </div>
+          </div>
+
+          <ul
+            className="list-group list-group-flush"
+            style={{ fontSize: "1.2rem" }}
+          >
+            <li className="list-group-item">
+              Total: {this.state.credit.total}
+            </li>
+            <li className="list-group-item">
+              Valor de las cuotas: {this.state.credit.valueCuote}
+            </li>
+            <li className="list-group-item">
+              Cuotas: {this.state.credit.cuotes}
+            </li>
+            <li className="list-group-item">
+              Vendedor: {this.state.credit.sellername}
+            </li>
+          </ul>
+        </div>
         <div>
-          <button className="btn btn-danger" onClick={() => this.LogOut()}>
+          <button className="btn btn-danger my-4" onClick={() => this.LogOut()}>
             Cerrar Sesión
           </button>
         </div>
